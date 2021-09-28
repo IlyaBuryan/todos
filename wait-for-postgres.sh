9412b71c6f313d32eb533d3cbad017526293f899
Ilya Buryan <burian-iliyar@yandex.ru>
2021-09-28 21:56:49 +0300

    5

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -7,7 +7,7 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD="dante123456" psql -h "$host" -d "library" -U "dante" -c '\q';>
+until PGPASSWORD="postgres" psql -h "$host" -d "postgres" -U "postgres" -c '\q';>
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
